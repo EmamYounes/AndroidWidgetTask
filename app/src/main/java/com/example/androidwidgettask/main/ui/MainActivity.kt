d68d715ed5b96bba58cd46e21f6142c61e4eef65
Emam <emamAhmed36@gmail.com>
2020-07-19 14:16:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/androidwidgettask/main/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidwidgettask.main.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidwidgettask.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
